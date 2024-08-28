2abe00c7041d08a1900dd86c14f65c4cdde93c53
Kactuar <decimator@yandex.ru>
2024-08-28 12:36:56 +0500

    upd 2

--- webapp/src/pages/MainPage/ui/MainPage.tsx
+++ webapp/src/pages/MainPage/ui/MainPage.tsx
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { HoroscopeSign } from 'widgets/HoroscopeSign';
 import signAll from 'shared/mock/sign';
 import { observer } from 'mobx-react-lite';
 import { useAppStore } from 'app/providers/StoreProvider/ui/StoreProvider';
-import { useTelegram } from 'shared/hooks/useTelegram';
 import cls from './MainPage.module.scss';
 
 const MainPage = observer(() => {
     const { signStore } = useAppStore();
-    const { user } = useTelegram();
-
-    useEffect(() => {
-        signStore.setLanguage(user?.language_code);
-    }, []);
 
     return (
         <div className={classNames(cls.SignBlock, {})}>
